73746507738fcdfa12a04a1fc399cc297281b485
ghormiere <guillaume.hormiere@telespazio.com>
2018-05-29 23:47:17 +0200

    Add log and try catch to handle error in image/tile generation

--- generator/sentinel2/sentinel_tile_producer.py
+++ generator/sentinel2/sentinel_tile_producer.py
@@ -3,11 +3,15 @@ Sentinel tile producer module
 """
 import os
 import tempfile
+import logging
 from threading import Thread
 from queue import Queue
 from .utils.sentinel_downloader import download_product_in_zone
 from .utils.tile_generator import get_x_y_for_lon_lat, create_raster_from_band, extract_tile, create_png_from_raster
 
+
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger("wtmse")
 
 class Tile:
     """
@@ -50,41 +54,44 @@ class SentinelImageProducer(Thread):
         Produce the big png image then send tile request to tile producer 
         """
         while True:
-            tile = SentinelImageProducer.tile_to_product.get()
-            zone_name = tile.zone_name
-            found_date = tile.found_date
-            file_path = tile.file_path
-            tile_bands = tile.bands
-            first_clip = tile.first_clip
-            second_clip = tile.second_clip
-            third_clip = tile.third_clip
+            try:
+                tile = SentinelImageProducer.tile_to_product.get()
+                zone_name = tile.zone_name
+                found_date = tile.found_date
+                file_path = tile.file_path
+                tile_bands = tile.bands
+                first_clip = tile.first_clip
+                second_clip = tile.second_clip
+                third_clip = tile.third_clip
 
-            if not os.path.isfile(file_path):
-                bands = download_product_in_zone(zone_name, found_date, tile_bands)
-                tiff_name = zone_name + "_" + \
-                    str(found_date.year) + "_" + \
-                    str(found_date.month) + "_" + str(found_date.day) + \
-                    "_"+str(tile_bands[0])+"_"+str(tile_bands[1])+"_"+str(tile_bands[2])
-                tiff_path = os.path.join(tempfile.gettempdir(), tiff_name)
+                if not os.path.isfile(file_path):
+                    bands = download_product_in_zone(zone_name, found_date, tile_bands)
+                    tiff_name = zone_name + "_" + \
+                        str(found_date.year) + "_" + \
+                        str(found_date.month) + "_" + str(found_date.day) + \
+                        "_"+str(tile_bands[0])+"_"+str(tile_bands[1])+"_"+str(tile_bands[2])
+                    tiff_path = os.path.join(tempfile.gettempdir(), tiff_name)
 
-                if not os.path.isfile(tiff_path):
-                    create_raster_from_band(
-                        bands[tile_bands[0]], bands[tile_bands[1]], bands[tile_bands[2]], tiff_path)
+                    if not os.path.isfile(tiff_path):
+                        create_raster_from_band(
+                            bands[tile_bands[0]], bands[tile_bands[1]], bands[tile_bands[2]], tiff_path)
 
-                big_png_name = zone_name + "_" + \
-                    str(found_date.year) + "_" + str(found_date.month) + \
-                    "_" + str(found_date.day) + \
-                    "_"+str(tile_bands[0])+"_"+str(tile_bands[1])+"_"+str(tile_bands[2])+ \
-                    "_"+str(first_clip[0])+"_"+str(first_clip[1])+ \
-                    "_"+str(second_clip[0])+"_"+str(second_clip[1])+ \
-                    "_"+str(third_clip[0])+"_"+str(third_clip[1])+ \
-                    ".png"
-                big_png_path = os.path.join(
-                    tempfile.gettempdir(), big_png_name)
-                if not os.path.isfile(big_png_path):
-                    create_png_from_raster(tiff_path, big_png_path, first_clip, second_clip, third_clip)
+                    big_png_name = zone_name + "_" + \
+                        str(found_date.year) + "_" + str(found_date.month) + \
+                        "_" + str(found_date.day) + \
+                        "_"+str(tile_bands[0])+"_"+str(tile_bands[1])+"_"+str(tile_bands[2])+ \
+                        "_"+str(first_clip[0])+"_"+str(first_clip[1])+ \
+                        "_"+str(second_clip[0])+"_"+str(second_clip[1])+ \
+                        "_"+str(third_clip[0])+"_"+str(third_clip[1])+ \
+                        ".png"
+                    big_png_path = os.path.join(
+                        tempfile.gettempdir(), big_png_name)
+                    if not os.path.isfile(big_png_path):
+                        create_png_from_raster(tiff_path, big_png_path, first_clip, second_clip, third_clip)
 
-                SentinelTileProducer.produce_request(tile)
+                    SentinelTileProducer.produce_request(tile)
+            except Exception as err:
+                LOGGER.error("Something wrong happen during image generation, maybe you should try to develop real code")
 
 class SentinelTileProducer(Thread):
     """
@@ -111,49 +118,52 @@ class SentinelTileProducer(Thread):
         Get tiles requests from the queue and treat it
         """
         while True:
-            tile = SentinelTileProducer.tile_to_product.get()
-            zone_name = tile.zone_name
-            found_date = tile.found_date
-            bbox = tile.bbox
-            file_path = tile.file_path
-            tile_bands = tile.bands
-            first_clip = tile.first_clip
-            second_clip = tile.second_clip
-            third_clip = tile.third_clip
+            try:
+                tile = SentinelTileProducer.tile_to_product.get()
+                zone_name = tile.zone_name
+                found_date = tile.found_date
+                bbox = tile.bbox
+                file_path = tile.file_path
+                tile_bands = tile.bands
+                first_clip = tile.first_clip
+                second_clip = tile.second_clip
+                third_clip = tile.third_clip
 
-            if not os.path.isfile(file_path):
-                tiff_name = zone_name + "_" + \
-                    str(found_date.year) + "_" + \
-                    str(found_date.month) + "_" + str(found_date.day) + \
-                    "_"+str(tile_bands[0])+"_"+str(tile_bands[1])+"_"+str(tile_bands[2])
-                tiff_path = os.path.join(tempfile.gettempdir(), tiff_name)
+                if not os.path.isfile(file_path):
+                    tiff_name = zone_name + "_" + \
+                        str(found_date.year) + "_" + \
+                        str(found_date.month) + "_" + str(found_date.day) + \
+                        "_"+str(tile_bands[0])+"_"+str(tile_bands[1])+"_"+str(tile_bands[2])
+                    tiff_path = os.path.join(tempfile.gettempdir(), tiff_name)
 
-                if not os.path.isfile(tiff_path):
-                    continue
+                    if not os.path.isfile(tiff_path):
+                        continue
 
-                big_png_name = zone_name + "_" + \
-                    str(found_date.year) + "_" + str(found_date.month) + \
-                    "_" + str(found_date.day) + \
-                    "_"+str(tile_bands[0])+"_"+str(tile_bands[1])+"_"+str(tile_bands[2])+ \
-                    "_"+str(first_clip[0])+"_"+str(first_clip[1])+ \
-                    "_"+str(second_clip[0])+"_"+str(second_clip[1])+ \
-                    "_"+str(third_clip[0])+"_"+str(third_clip[1])+ \
-                    ".png"
-                big_png_path = os.path.join(
-                    tempfile.gettempdir(), big_png_name)
-                if not os.path.isfile(big_png_path):
-                    continue
+                    big_png_name = zone_name + "_" + \
+                        str(found_date.year) + "_" + str(found_date.month) + \
+                        "_" + str(found_date.day) + \
+                        "_"+str(tile_bands[0])+"_"+str(tile_bands[1])+"_"+str(tile_bands[2])+ \
+                        "_"+str(first_clip[0])+"_"+str(first_clip[1])+ \
+                        "_"+str(second_clip[0])+"_"+str(second_clip[1])+ \
+                        "_"+str(third_clip[0])+"_"+str(third_clip[1])+ \
+                        ".png"
+                    big_png_path = os.path.join(
+                        tempfile.gettempdir(), big_png_name)
+                    if not os.path.isfile(big_png_path):
+                        continue
 
-                top_left = get_x_y_for_lon_lat(
-                    tiff_path, bbox[0][0], bbox[0][1])
-                top_rigth = get_x_y_for_lon_lat(
-                    tiff_path, bbox[0][0], bbox[1][1])
-                bottom_left = get_x_y_for_lon_lat(
-                    tiff_path, bbox[1][0], bbox[0][1])
-                bottom_right = get_x_y_for_lon_lat(
-                    tiff_path, bbox[1][0], bbox[1][1])
-                extract_tile(big_png_path, top_left, top_rigth,
-                             bottom_left, bottom_right, file_path)
+                    top_left = get_x_y_for_lon_lat(
+                        tiff_path, bbox[0][0], bbox[0][1])
+                    top_rigth = get_x_y_for_lon_lat(
+                        tiff_path, bbox[0][0], bbox[1][1])
+                    bottom_left = get_x_y_for_lon_lat(
+                        tiff_path, bbox[1][0], bbox[0][1])
+                    bottom_right = get_x_y_for_lon_lat(
+                        tiff_path, bbox[1][0], bbox[1][1])
+                    extract_tile(big_png_path, top_left, top_rigth,
+                                bottom_left, bottom_right, file_path)
+            except Exception as err:
+                LOGGER.error("Something wrong happen during tile generation, maybe you should try to develop real code")
 
 
 SENTINEL_PRODUCER_INSTANCE = SentinelImageProducer()
